5c43e78aed04b5f426759b17db7abd7756fac939
Rubel Mahmud <vxlrubel@gmail.com>
2025-08-31 03:55:35 +0600

    create index file for initiate

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<div class="container">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam modi incidunt vero hic non aliquam amet aperiam sed at, voluptatibus delectus dignissimos distinctio sapiente veritatis autem magni unde, ipsa quasi.
+</div>
+<div>
+    asdflkj
+    <div>laksdfj <div><span class="dashicons dashicons-menu-alt2"></span></div>
+
+        <span class="dashicons dashicons-no-alt"></span>
+    </div>
+</div>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit inventore aliquam sequi fugiat, non tempore itaque iure quis officiis dignissimos delectus! Eos, veritatis dolore sequi explicabo atque magni corrupti dignissimos!</p>
+
+
+
+<?php get_footer(); ?>
+
+<footer>
+    &copy; 2024 VelixTech. All rights reserved.
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
