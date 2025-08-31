8b2fe481af4e380fd40da20e0235ce2c758cd735
Rubel Mahmud <vxlrubel@gmail.com>
2025-09-01 03:11:06 +0600

    update main file structure.

--- index.php
+++ index.php
@@ -1,25 +1,11 @@
+<!-- get header content -->
 <?php get_header(); ?>
-<div class="container">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam modi incidunt vero hic non aliquam amet aperiam sed at, voluptatibus delectus dignissimos distinctio sapiente veritatis autem magni unde, ipsa quasi.
-</div>
-<div>
-    asdflkj
-    <div>laksdfj <div><span class="dashicons dashicons-menu-alt2"></span></div>
-
-        <span class="dashicons dashicons-no-alt"></span>
-    </div>
-</div>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit inventore aliquam sequi fugiat, non tempore itaque iure quis officiis dignissimos delectus! Eos, veritatis dolore sequi explicabo atque magni corrupti dignissimos!</p>
 
 
+<!-- this is the main content -->
+<main id="app" v-cloak>
 
+</main>
+
+<!-- get footer content -->
 <?php get_footer(); ?>
-
-<footer>
-    &copy; 2024 VelixTech. All rights reserved.
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-
-</html>
