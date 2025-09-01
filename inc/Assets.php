73fcffd2b57d225859253dd98bab46b81114d161
Rubel Mahmud <vxlrubel@gmail.com>
2025-09-01 23:54:38 +0600

    remove unncessary code for logged out user.

--- inc/Assets.php
+++ inc/Assets.php
@@ -10,7 +10,17 @@ class Assets
     {
         // Frontend assets
         add_action('wp_enqueue_scripts', [$this, 'register_frontend_script']);
+
+        // remove emoji script
+        add_action('init', [$this, 'remove_emoji_script']);
     }
+
+    public function remove_emoji_script()
+    {
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('wp_print_styles', 'print_emoji_styles');
+    }
+
 
     public function register_frontend_script()
     {
@@ -37,6 +47,14 @@ class Assets
                 THEME_VERSION,
                 true
             );
+        }
+
+        // Remove Gutenberg styles for non-logged in users
+        if (! is_user_logged_in()) {
+            wp_dequeue_style('wp-block-library');
+            wp_dequeue_style('wp-block-library-theme');
+            wp_dequeue_style('global-styles');
+            wp_dequeue_style('classic-theme-styles');
         }
     }
 
