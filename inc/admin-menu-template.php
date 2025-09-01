5615454b91414487fc1f949d071543a30487227e
Rubel Mahmud <vxlrubel@gmail.com>
2025-09-02 03:22:44 +0600

    admin menu render page.

---
+++ inc/admin-menu-template.php
@@ -0,0 +1,16 @@
+<div class="wrap" id="admin-app">
+
+    <h1>VelixTech Admin Page</h1>
+    <hr />
+    <div>
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+            <router-link to="/contact">Contact</router-link>
+        </nav>
+        <hr />
+        <!-- Router View -->
+        <router-view></router-view>
+    </div>
+
+</div>
