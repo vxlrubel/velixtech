199c9ee44792ddfb7bacb7f204f40be694648368
Rubel Mahmud <vxlrubel@gmail.com>
2025-09-03 01:52:42 +0600

    create home page.,

---
+++ page-homepage.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Home Page
+Description: A custom template for displaying homepage.
+*/
+
+get_header();
+?>
+
+<div class="container">
+
+    <h1>Homepage</h1>
+
+</div>
+
+<?php
+get_footer();
