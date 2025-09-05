f6aefa445e5293a573c5d1d738200daa3ede95b4
Rubel Mahmud <vxlrubel@gmail.com>
2025-09-05 20:52:07 +0600

    update

--- page-homepage.php
+++ page-homepage.php
@@ -7,10 +7,26 @@ Description: A custom template for displaying homepage.
 get_header();
 ?>
 
-<div class="container">
+<div class="clearfix">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div>
+                    <div>here is the small title</div>
+                    <div>Big Title Here</div>
+                    <div>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora numquam, et adipisci officiis excepturi amet natus nobis, eaque exercitationem quibusdam aspernatur ad! Culpa, fugit minima!
+                    </div>
 
-    <h1>Homepage</h1>
-
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="ratio-16x9">
+                    <img src="<?php echo ASSETS . 'img/hero-image.jpg' ?>" class="mix-blend-mode-darken" alt="hero image">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
